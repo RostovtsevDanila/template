8c2f10b7c3b7ab46ee3d95d1aa6f966ad879d969
Aleksey <a.tatarov@omprussia.ru>
2019-11-20 18:04:32 +0300

    Add printWord method

--- templateclass.cpp
+++ templateclass.cpp
@@ -1,6 +1,12 @@
 #include "templateclass.h"
+#include <QDebug>
 
 TemplateClass::TemplateClass(QObject *parent):QObject (parent)
 {
 
+}
+
+void TemplateClass::printWord(const QString &word)
+{
+    qDebug() << word;
 }
--- templateclass.h
+++ templateclass.h
@@ -6,6 +6,8 @@ class TemplateClass : public QObject
 {
 public:
     TemplateClass(QObject *parent = nullptr);
+
+    void printWord(const QString &word);
 };
 
 #endif // TEMPLATECLASS_H
